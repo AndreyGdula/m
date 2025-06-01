e4de30ab6b0c80c1de906e9d8a6a852942104adf
Andrey Gdula <andreygdula@gmail.com>
2025-06-01 05:39:34 -0300

    iniciando a interação

--- menu/script/script.js
+++ menu/script/script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   const splash = document.getElementById('splash-screen');
-  const main = document.getElementById('main-content');
+  const main = document.querySelectorAll("div.main-content")
   const particles = document.getElementById('particles')
   const papiro = document.getElementById('papiro')
   const timeout = 500
@@ -12,7 +12,7 @@ window.onload = () => {
     setTimeout(() => {
       splash.style.display = 'none';
       particles.style.display = 'block'
-      main.style.display = 'block';
+      main.forEach(main => main.style.display = 'block')
       papiro.classList.add('visible')
     }, 800);
   }, timeout);
